234f5305fc6e82b7359ffb458ff571c640a20111
もっちゃん <s7056i3056@gmail.com>
2024-04-07 23:59:38 +0900

    feat: make greet

---
+++ packages/backend/src/modules/root/greet/greet.resolvers.ts
@@ -0,0 +1,11 @@
+import { MyContext } from "../../../types/graphql.js";
+
+export const resolvers = {
+  Query: {
+    greet: async (_: any, args: any, context: MyContext, info: any) => {
+      return 'hello';
+    },
+  },
+};
+
+export { MyContext };
