4bd6b38a8752bb08c5b6e5865e73451d80050f7e
もっちゃん <s7056i3056@gmail.com>
2024-04-14 23:57:24 +0900

    要素を各componentに切り出した

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Card } from "./components/Card";
+import { Logo } from "./components/Logo";
+import { ReadTheDocs } from "./components/ReadTheDocs";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Logo />
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Card />
+      <ReadTheDocs />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ packages/frontend/src/components/ReadTheDocs.tsx
@@ -0,0 +1,7 @@
+export const ReadTheDocs = () => {
+  return (
+    <p className="read-the-docs">
+      Click on the Vite and React logos to learn more
+    </p>
+  );
+};
