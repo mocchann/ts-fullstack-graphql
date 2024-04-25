d9c3f900c14c3ede405723c48473f0ae6c229f19
もっちゃん <s7056i3056@gmail.com>
2024-04-25 23:44:05 +0900

    feat: filtering practice

---
+++ packages/frontend/src/components/filtering.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { foods, filterItems } from "./data.js";
+
+export default function FilterableList() {
+  const [query, setQuery] = useState("");
+
+  function handleChange(e) {
+    setQuery(e.target.value);
+  }
+
+  const newFoods = filterItems(foods, query);
+
+  return (
+    <>
+      <SearchBar query={query} handleChange={handleChange} />
+      <hr />
+      <List items={newFoods} />
+    </>
+  );
+}
+
+function SearchBar({ query, handleChange }) {
+  return (
+    <label>
+      Search: <input value={query} onChange={handleChange} />
+    </label>
+  );
+}
+
+function List({ items }) {
+  return (
+    <table>
+      <tbody>
+        {items.map((food) => (
+          <tr key={food.id}>
+            <td>{food.name}</td>
+            <td>{food.description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
