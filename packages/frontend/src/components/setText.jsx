a802a3a749cb5efce4423e5d650cb908fc966193
もっちゃん <s7056i3056@gmail.com>
2024-04-26 15:00:00 +0900

    practice set text

---
+++ packages/frontend/src/components/setText.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function App() {
+  const [showHint, setShowHint] = useState(false);
+  const [text, setText] = useState("");
+
+  if (showHint) {
+    return (
+      <div>
+        <p>
+          <i>Hint: Your favorite city?</i>
+        </p>
+        <Form />
+        <button
+          onClick={() => {
+            setShowHint(false);
+          }}
+        >
+          Hide hint
+        </button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Form text={text} onChange={(e) => setText(e.target.value)} />
+      <button
+        onClick={() => {
+          setShowHint(true);
+        }}
+      >
+        Show hint
+      </button>
+    </div>
+  );
+}
+
+function Form({ text, onChange }) {
+  return <textarea value={text} onChange={onChange} />;
+}
