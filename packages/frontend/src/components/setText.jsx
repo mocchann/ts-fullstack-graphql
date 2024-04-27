0efed6c2c109b50172d4e5f9502d72bb39c27461
もっちゃん <s7056i3056@gmail.com>
2024-04-27 10:14:17 +0900

    fix: ボタンをクリックしてもフォームのtextがリセットされないように修正

--- packages/frontend/src/components/setText.jsx
+++ packages/frontend/src/components/setText.jsx
@@ -1,40 +1,35 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function App() {
   const [showHint, setShowHint] = useState(false);
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
 
   if (showHint) {
     return (
       <div>
-        <p>
-          <i>Hint: Your favorite city?</i>
-        </p>
-        <Form />
-        <button
-          onClick={() => {
-            setShowHint(false);
-          }}
-        >
-          Hide hint
-        </button>
+        <p><i>Hint: Your favorite city?</i></p>
+        <Form　text={text} onChange={e => setText(e.target.value)} />
+        <button onClick={() => {
+          setShowHint(false);
+        }}>Hide hint</button>
       </div>
     );
   }
   return (
     <div>
-      <Form text={text} onChange={(e) => setText(e.target.value)} />
-      <button
-        onClick={() => {
-          setShowHint(true);
-        }}
-      >
-        Show hint
-      </button>
+      <Form　text={text} onChange={e => setText(e.target.value)} />
+      <button onClick={() => {
+        setShowHint(true);
+      }}>Show hint</button>
     </div>
   );
 }
 
 function Form({ text, onChange }) {
-  return <textarea value={text} onChange={onChange} />;
+  return (
+    <textarea
+      value={text}
+      onChange={onChange}
+    />
+  );
 }
