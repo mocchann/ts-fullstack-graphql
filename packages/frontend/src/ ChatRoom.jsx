9d92a9a0490817b6d9b65f45dd9f07e0a05ded1f
もっちゃん <s7056i3056@gmail.com>
2024-05-02 09:38:16 +0900

    refactor: change reactive value createConnection to isEncrypted

--- packages/frontend/src/ ChatRoom.jsx
+++ packages/frontend/src/ ChatRoom.jsx
@@ -1,12 +1,18 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import {
+  createEncryptedConnection,
+  createUnencryptedConnection,
+} from './chat.js';
 
-export default function ChatRoom({ roomId, createConnection }) {
+export default function ChatRoom({ roomId, isEncrypted }) {
   useEffect(() => {
+    const createConnection = isEncrypted ?
+      createEncryptedConnection :
+      createUnencryptedConnection;
     const connection = createConnection(roomId);
     connection.connect();
     return () => connection.disconnect();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [roomId, createConnection]);
+  }, [roomId, isEncrypted]);
 
   return <h1>Welcome to the {roomId} room!</h1>;
 }
