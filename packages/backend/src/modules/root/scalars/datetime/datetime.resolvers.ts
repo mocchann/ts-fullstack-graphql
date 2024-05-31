d89b32a00794c419d16496e17a5a48047288c8ae
もっちゃん <s7056i3056@gmail.com>
2024-05-31 23:48:18 +0900

    feat: 自作したスカラーをライブラリを使う形に変更

---
+++ packages/backend/src/modules/root/scalars/datetime/datetime.resolvers.ts
@@ -0,0 +1,6 @@
+import { Resolvers } from "../../../../__generated__/graphql.js";
+import { DateTimeResolver } from "graphql-scalars";
+
+export const resolvers: Resolvers = {
+  DateTime: DateTimeResolver,
+};
