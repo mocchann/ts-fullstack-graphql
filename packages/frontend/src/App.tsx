40a7048cc4cb4dc4b919fdfd9288f3325b2d6658
もっちゃん <s7056i3056@gmail.com>
2024-06-01 10:27:46 +0900

    feat: frontからgraphqlでデータ取得、表示できるようにした

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,8 +1,28 @@
+import { gql, useQuery } from "@apollo/client";
+
+const getTodosQuery = gql`
+  query GetTodos {
+    getTodos {
+      todos {
+        id
+        title
+        isCompleted
+        createdAt
+        updatedAt
+      }
+    }
+  }
+`;
+
 export default function App() {
+  const { data, loading, error } = useQuery(getTodosQuery);
+
   return (
     <div className="App">
       <div>
-        <h1 className="bg-red-600">hello world</h1>
+        {data?.getTodos?.todos.map((item: any) => (
+          <p key={item.id}>{item.title}</p>
+        ))}
       </div>
     </div>
   );
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:5555/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
