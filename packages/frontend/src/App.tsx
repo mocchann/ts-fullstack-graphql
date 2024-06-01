cff4e4a28d53bbb6e7f82f985e305582bf4c4d97
もっちゃん <s7056i3056@gmail.com>
2024-06-01 23:51:52 +0900

    feat: tailwindcssが動作していることを確認するために仮でcssをあてる

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,17 +1,20 @@
 import { useTodosQuery } from "./__generated__/graphql";
+import { Layout } from "./components/Layout/Layout";
 
 export default function App() {
   const { data, loading, error } = useTodosQuery();
 
   return (
-    <div className="App">
-      <div>
-        {data?.getTodos?.todos?.map((item) => (
-          <p key={item?.id}>
-            {item?.title} {item?.createdAt.toISOString()}
-          </p>
-        ))}
+    <Layout>
+      <div className="max-w-xl mx-auto p-7 bg-red-200">
+        <div>
+          {data?.getTodos?.todos?.map((item) => (
+            <p key={item?.id}>
+              {item?.title} {item?.createdAt.toISOString()}
+            </p>
+          ))}
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
---
+++ packages/frontend/src/components/Layout/Layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Layout = ({ children }: Props): JSX.Element => {
+  return (
+    <>
+      <div className="h-screen bg-gray-100">{children}</div>
+    </>
+  );
+};
