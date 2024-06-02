db687247484ad6bbdb09b939ff9b17f4c7522a84
もっちゃん <s7056i3056@gmail.com>
2024-06-02 14:38:00 +0900

    add: ドロップダウンメニューのデザインを作成

---
+++ packages/frontend/src/components/Elements/DropDownMenu/DropDownMenu.tsx
@@ -0,0 +1,45 @@
+import { ReactNode, useState } from "react";
+
+type Props = {
+  clickTarget: ReactNode;
+  menuItems: ReactNode[];
+};
+
+export const DropDownMenu = ({
+  clickTarget,
+  menuItems,
+}: Props): JSX.Element => {
+  const [isMenuActive, setIsMenuActive] = useState<boolean>(false);
+
+  const toggleMenu = () => setIsMenuActive(!isMenuActive);
+
+  return (
+    <div className="relative">
+      <button onClick={() => setIsMenuActive(!isMenuActive)}>
+        {clickTarget}
+      </button>
+      {isMenuActive ? (
+        <>
+          <button
+            tabIndex={-1}
+            onClick={toggleMenu}
+            className="fixed inset-0 z-10 cursor-default"
+          ></button>
+          <div className="absolute right-0 z-20 bg-white shadow border border-gray-100 py-2 px-4 mt-2 rounded-lg w-48">
+            <ul>
+              {menuItems.map((item, index) => (
+                <li
+                  onClick={toggleMenu}
+                  key={index}
+                  className="text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg px-4 py-2"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ) : null}
+    </div>
+  );
+};
