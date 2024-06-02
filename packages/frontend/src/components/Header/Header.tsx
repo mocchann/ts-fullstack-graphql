74077d2ef53a9910d6e467bdd2a45ebb86314a9a
もっちゃん <s7056i3056@gmail.com>
2024-06-02 11:33:36 +0900

    feat: Headerコンポーネントを作成

---
+++ packages/frontend/src/components/Header/Header.tsx
@@ -0,0 +1,24 @@
+import { BsMenuButton, BsMoon, BsSun } from "react-icons/bs";
+
+type Props = {};
+
+export const Header = ({}: Props): JSX.Element => {
+  const isDarkMode = false;
+
+  return (
+    <header className="px-6 py-5 border-b bg-white">
+      <div className="flex justify-between">
+        <section>
+          <span className="text-slate-600 font-semibold">Todo List</span>
+        </section>
+        <section>
+          <ul>
+            <li>
+              <button>{isDarkMode ? <BsSun /> : <BsMoon />}</button>
+            </li>
+          </ul>
+        </section>
+      </div>
+    </header>
+  );
+};
